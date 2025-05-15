c88801b76a16fd1938a6a0376d01a89d51e6a4ff
lehuuloc <lehuuloc.qt@gmail.com>
2025-05-15 19:40:47 +0700

    add try/catch feature

---
+++ src/emiri/StringNumberConverter.java
@@ -0,0 +1,27 @@
+package emiri;
+
+import java.util.Scanner;
+
+public class StringNumberConverter {
+
+    public static void main(String[] args) {
+        int number = inputNumber();
+        System.out.println("So ban da nhap: " + number);
+    }
+
+    private static int inputNumber() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hay nhap ky tu bat ky:");
+        String numberString = scanner.nextLine();
+        System.out.println("Chuoi vua nhap: " + numberString);
+
+        try {
+            int number = Integer.parseInt(numberString); //NumberFormatException
+            System.out.println("Number thu duoc: " + number);
+            return number;
+        } catch (NumberFormatException exception) {
+            System.out.println("Cai thu may vua nhap vao ko phai la Number");
+            return inputNumber();
+        }
+    }
+}
