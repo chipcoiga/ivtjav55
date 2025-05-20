332c7a8043aa9793ad336e0374664156ff63e0ac
lehuuloc <lehuuloc.qt@gmail.com>
2025-05-20 20:26:01 +0700

    bai 1 den bai 3

---
+++ src/domain/HocSinhDto.java
@@ -0,0 +1,58 @@
+package domain;
+
+public class HocSinhDto {
+    private String name;
+    private String address;
+    private int age;
+    private float math;
+    private float physical;
+    private float chemistry;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public float getMath() {
+        return math;
+    }
+
+    public void setMath(float math) {
+        this.math = math;
+    }
+
+    public float getPhysical() {
+        return physical;
+    }
+
+    public void setPhysical(float physical) {
+        this.physical = physical;
+    }
+
+    public float getChemistry() {
+        return chemistry;
+    }
+
+    public void setChemistry(float chemistry) {
+        this.chemistry = chemistry;
+    }
+}
