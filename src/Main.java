0cd3aacadd203c2edfacd50ec90441dd451647d9
lehuuloc <lehuuloc.qt@gmail.com>
2025-05-20 20:26:01 +0700

    project contructor

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import domain.HocSinhDto;
+import service.HocSinhService;
+
 public class Main {
     public static void main(String[] args) {
+        //Gia dinh da co hoc sinh
+        HocSinhDto hocSinhDto = new HocSinhDto();
+        hocSinhDto.setName("Nguyen Van A");
+        hocSinhDto.setAddress("102 Quang Trung");
+        hocSinhDto.setMath(5);
+        hocSinhDto.setPhysical(6);
+        hocSinhDto.setChemistry(7);
 
-        System.out.println("\ta\tb\tc");  //a b c
-        System.out.println("\\    \\"); // \\\
-        System.out.println("'"); // '
-        System.out.println("\"\"\""); // """
-        System.out.println("C:\nin\the downward spiral");
+        HocSinhService service = new HocSinhService();
+        boolean isHocSinhValid = service.validateHocSinh(hocSinhDto);
+
+        // if (isHocSinhValid ==false ) {
+        if (!isHocSinhValid) {
+            System.out.println("Thong tin hoc sinh ko hop le");
+            return;
+        }
+
+        String hocLuc = service.phanLoaiHocLuc(hocSinhDto);
+        String name = hocSinhDto.getName();
+        System.out.println("Hoc luc cua Hoc sinh ten "
+                + name + " la: " + hocLuc);
     }
 }
--- src/emiri/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package emiri;
-
-public class HelloWorld {
-}
-
-//git clone https://github.com/chipcoiga/ivtjav55.git
-//git pull
