647c8937f75a9065674bce059f2d9ee0c905a185
lehuuloc <lehuuloc.qt@gmail.com>
2025-05-21 19:45:13 +0700

    init

---
+++ src/com/demo/javcore/helper/StringHelper.java
@@ -0,0 +1,23 @@
+package com.demo.javcore.helper;
+
+public class StringHelper {
+    public static boolean isBlank(String input) {
+        if (input == null) {
+            System.out.println("Input bi null");
+            return false;
+        }
+
+        if (input.equals("")) {
+            System.out.println("Input bi empty");
+            return false;
+        }
+
+        String addressStrim = input.replaceAll(" ", "");
+        if (addressStrim.equals("")) {
+            System.out.println("Input bi empty");
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ src/com/demo/javcore/service/HocSinhService.java
@@ -0,0 +1,48 @@
+package com.demo.javcore.service;
+
+
+import com.demo.javcore.domain.HocSinhDto;
+import com.demo.javcore.helper.StringHelper;
+
+public class HocSinhService {
+
+    /**
+     * Dưới 4 => yếu
+     * Từ 4 đến dưới 6 => trung bình
+     * Từ 6 đến dưới 8 => khá
+     * từ 8 trở lên => Giỏi
+     * @param hocSinh
+     * @return
+     */
+    public String phanLoaiHocLuc(HocSinhDto hocSinh) {
+        float diemTrungBinh =
+                (hocSinh.getMath()
+                        + hocSinh.getPhysical()
+                        + hocSinh.getChemistry())/3;
+
+        if (diemTrungBinh < 4) {
+            return "Học lực yếu";
+        }
+
+        if (diemTrungBinh < 6) {
+            return "Học lực trung bình";
+        }
+
+        if (diemTrungBinh < 8) {
+            return "Học lực Khá";
+        }
+
+        return "Học lực GIỎI";
+    }
+
+    public boolean validateHocSinh(HocSinhDto hocSinh) {
+        boolean isNameValid = StringHelper.isBlank(hocSinh.getName());
+        boolean isAddressValid = StringHelper.isBlank(hocSinh.getAddress());
+
+        if (isNameValid == false || isAddressValid == false) {
+            return false;
+        }
+
+        return true;
+    }
+}
