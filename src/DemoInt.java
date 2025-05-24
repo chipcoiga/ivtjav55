114e2d55a706cd5a1fd0451eca73fb46f1697faa
lehuuloc <lehuuloc.qt@gmail.com>
2025-05-24 18:01:26 +0700

    demo int

---
+++ src/DemoInt.java
@@ -0,0 +1,9 @@
+public class DemoInt {
+    public static void main(String[] args) {
+
+        for (int i = Integer.MAX_VALUE - 5; i > 0; ) {
+            i++;
+            System.out.println(i);
+        }
+    }
+}
